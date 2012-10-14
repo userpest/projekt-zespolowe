a4710bfcc749efe36a42e864b5ca8b73970dfbfa
uzyszkodnik <uzyszkodnik.reg@gmail.com>
2012-10-14 14:29:54 +0200

    unfinished collision detection

--- src/board.py
+++ src/board.py
@@ -15,16 +15,91 @@ class GameMap:
 		pass
 
 class Board:
-	def __init__(self):
-		pass
+	def __init__(self,game_map):
+		self.game_map = game_map
+		self.objects =  [] 
 
 	def epoch(self,):
+		"""call me every frame"""
+		self._handleObjectCollisions()
+		self._handleTerrainCollisions()
+		self._handleMovement()
+
+
+	def register(self, obj):
+		"""registers and object in the engine"""
+		self.objects.append(obj)
 		pass
+
+	def unregister(self, obj):
+		"""unregisters an object from the engine """
+		self.objects.remove(obj)
+		pass
+
+
+	#TODO: OPTIMIZE
+	def _handleObjectCollisions(self):
+		for i in self.objects:
+			for j in self.objects:
+				if self._rectsCollison(i,j) and self._colMapCollision(i,j):
+					pass
+
+
+	def _handleTerrainCollisions(self):
+		for i in self.objects:
+			if self._terrainCollision(i):
+				if i._handleTerrainImpact():
+					pass
+				else:
+					pass
+
+
+	def _handleMovement():
+		pass
+
+	def _rectsCollison(self,obj1, obj2):
+		if (obj1.collision_map.get_width() + obj1.x < obj2.x 
+			or
+		    obj1.x > obj2.collision_map.get_width() + obj2.x  
+			or 
+	       	    obj1.collision_map.get_height()+obj1.y < obj2.y 
+		   	or 
+		    obj1.y > obj2.collision_map.get_height() + obj2.y ):
+
+			return False
+
+		return True
+	#TODO optimize
+	def _colMapCollision(self,obj1,obj2):
+		#calculate the overlap coords of the rectangles
+
+		if obj1.collision_map.get_width()+obj1.x > obj2.x:
+			collision_start_x = obj2.x
+			collision_end_x = obj1.collision_map.get_width()+obj1.x
+		else:
+			collision_start_x = obj1.x
+			collision_end_x = obj2.collision_map.get_width()+obj2.x
+
+		if obj1.collision_map.get_height()+obj1.y > obj2.y:
+			collision_start_y = obj2.y
+			collision_end_y = obj1.collision_map.get_height()+obj1.y
+		else:
+			collision_start_y = obj1.y
+			collision_end_y = obj2.collision_map.get_height()+obj2.y
+
+		for i in range(
+
+
+	def _terrainCollision(self,obj1):
+		pass
+
+
 
 
 class BoardObject:
 
 	def __init__(self,x=None,y=None,v=Vector2D(),collision_map=None,mass=1):
+		self.obj_id=0
 		self.v = v
 		self.mass=mass
 		self.x = x
